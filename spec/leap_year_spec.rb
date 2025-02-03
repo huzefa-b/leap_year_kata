b950d5db20a56c3a6a0b7c3f40091cbfdb189460
huzefa <huzefa.b@incubyte.co>
2025-02-03 21:32:52 +0530

    refactored code both specs and logic

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -1,28 +1,30 @@
 require 'leap_year'
 
 RSpec.describe LeapYear do
+  let(:calculator) { LeapYear.new }
+
   describe '#leap_year?' do
     context 'when year is not divisible by 4' do
       it 'returns false' do
-        expect(LeapYear.new.leap_year?(1997)).to be false
+        expect(calculator.leap_year?(1997)).to be false
       end
     end
 
     context 'when year is divisible by 4' do
       it 'returns true' do
-        expect(LeapYear.new.leap_year?(1996)).to be true
+        expect(calculator.leap_year?(1996)).to be true
       end
     end
 
     context 'when year is divisible by 100 but not by 400' do
       it 'returns false' do
-        expect(LeapYear.new.leap_year?(1800)).to be false
+        expect(calculator.leap_year?(1800)).to be false
       end
     end
 
     context 'when year is divisible by 400' do
       it 'returns true' do
-        expect(LeapYear.new.leap_year?(1600)).to be true
+        expect(calculator.leap_year?(1600)).to be true
       end
     end
   end
