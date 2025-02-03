9091ab5d4b8dc551f4888d2c5752950cc30c2a3f
huzefa <huzefa.b@incubyte.co>
2025-02-03 21:10:52 +0530

    Add failing spec when year is divisible by 4

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -1,10 +1,16 @@
 require 'leap_year'
 
-RSpec.describe LeapYear do  
+RSpec.describe LeapYear do
   describe '#leap_year?' do
     context 'when year is not divisible by 4' do
       it 'returns false' do
         expect(LeapYear.new.leap_year?(1997)).to be false
+      end
+    end
+
+    context 'when year is divisible by 4' do
+      it 'returns true' do
+        expect(calculator.leap_year?(1996)).to be true
       end
     end
   end
