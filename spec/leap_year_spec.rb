fd65bb037e745f58db3bf8b7ba9dde0fe8c9b2db
huzefa <huzefa.b@incubyte.co>
2025-02-03 18:18:32 +0530

    Add failing scenario for a year not divisible by 4 should return false

---
+++ spec/leap_year_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe LeapYear do  
+  describe '#leap_year?' do
+    context 'when year is not divisible by 4' do
+      it 'returns false' do
+        expect(LeapYear.new.leap_year?(1997)).to be false
+      end
+    end
+  end
+end
